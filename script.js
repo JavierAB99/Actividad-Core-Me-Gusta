e2ab2c66edbe180c75cbb71a3f11c742f884fce0
JavierAB99 <javierarceburgos@gmail.com>
2024-09-12 14:49:59 -0300

    Se agrego comentario

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var botonLinn = document.querySelector("#boton-linn");
-var botonLina = document.querySelector("#boton-lina");
+var botonLina = document.querySelector("#boton-lina"); //Selectores de botones de like
 var botonMateo = document.querySelector("#boton-mateo");
 
-var likesLinn = document.getElementById("#likes-linn");
-var txtLikesLinn = document.querySelector("#likes-linn");
+var likesLinn = document.getElementById("#likes-linn"); //Para conseguir el valor numerico del texto de cantidad de likes
+var txtLikesLinn = document.querySelector("#likes-linn"); //Selector para el texto de likes que sera modificado
 
-botonLinn.addEventListener("click", function (){
-    likesLinn++;
-    txtLikesLinn.innerText = likesLinn;
+botonLinn.addEventListener("click", function (){ //Funcion onclick para el boton de likes de Linn y mas adelante de los otros usuarios
+    likesLinn++; //Aumenta cantidad en 1
+    txtLikesLinn.innerText = likesLinn; //Se cambia el texto por el nuevo valor
 });
 
 var likesLina = document.getElementById("#likes-lina");
